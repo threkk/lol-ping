f8cc0a403006b025957f02eeefcf83d8ffd56e8d
Alberto Mtnz <me@albertommr.com>
2016-04-25 03:13:07 +0200

    Adds the ping function and the servers ip.

---
+++ src/lol_ping.py
@@ -0,0 +1,54 @@
+"""
+TODO: Add docstring
+"""
+import subprocess
+import re
+from platform import system
+
+# Extracted from
+# https://www.reddit.com/r/leagueoflegends/comments/4efy17/how_to_check_your_ping_without_getting_into_the/d20167p
+SERVER = {
+    'NA': '104.160.131.3',
+    'EUW': '104.160.141.3',
+    'EUNE': '104.160.142.3',
+    'OCE': '104.160.156.1',
+    'LAN': '104.160.136.3'
+}
+
+
+def ping(host):
+    """
+    Calls the native ping function of OS X to extract the average time, the
+    maximum time and the unit of the connection to the given server.
+
+    :param host: str IP of the server.
+    :return: dictionary average time, maximum time
+    """
+
+    # We call the native ping command from OS X.
+    cmd = subprocess.Popen(
+        ['ping', '-c', '4', host],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        universal_newlines=True
+    )
+
+    out, err = cmd.communicate()
+
+    # Invalid host
+    if err is not '':
+        return []
+
+    else:
+        # Grabs the average, max and the unit
+        regexp = re.compile(r'round-trip min/avg/max/stddev ='
+                            r' \d+.\d+/(\d+.\d+)/(\d+.\d+)/\d+.\d+ (\w+)$')
+        match = re.search(regexp, out)
+
+        return match.group(1, 2, 3) if match else []
+
+if __name__ == '__main__':
+    if system().lower() == 'darwin':
+        print(ping(SERVER['EUW']))
+    else:
+        print(system().lower())
